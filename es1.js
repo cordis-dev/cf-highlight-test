ff3682a8f795f79968ab2c539031ac674b022486
cordis-dev <cordis.dev@gmail.com>
2016-09-14 09:42:50 +0300

    Create es1.js

---
+++ es1.js
@@ -0,0 +1,18 @@
+/*!
+ * Test
+ * Licensed under the MIT license
+ */
+ 
+    _.partial = function (func) {
+        var boundArgs = slice.call(arguments, 1);
+        var bound = function () {
+            var position = 0, length = boundArgs.length;
+            var args = Array(length);
+            for (var i = 0; i < length; i++) {
+                args[i] = boundArgs[i] === _ ? arguments[position++] : boundArgs[i];
+            }
+            while (position < arguments.length) args.push(arguments[position++]);
+            return executeBound(func, bound, this, this, args);
+        };
+        return bound;
+    };
